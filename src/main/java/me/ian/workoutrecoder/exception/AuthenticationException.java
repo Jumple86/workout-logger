cb3cb809304cfceaa642dc081eacc0bc5d4e66bc
Ian <ianyu@simbo.com.tw>
2024-07-08 23:37:39 +0800

    feat: add constructor

--- src/main/java/me/ian/workoutrecoder/exception/AuthenticationException.java
+++ src/main/java/me/ian/workoutrecoder/exception/AuthenticationException.java
@@ -1,4 +1,11 @@
 package me.ian.workoutrecoder.exception;
 
 public class AuthenticationException extends RuntimeException {
+    public AuthenticationException() {
+
+    }
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
 }
