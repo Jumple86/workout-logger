5c6ba1cfcce1ba3dbde104732dd7864dba75456f
Ian <ianyu@simbo.com.tw>
2024-07-21 23:01:02 +0800

    feat: check menu is weekly when day is not empty

--- src/main/java/me/ian/workoutrecoder/service/impl/WorkoutMenuContentServiceImpl.java
+++ src/main/java/me/ian/workoutrecoder/service/impl/WorkoutMenuContentServiceImpl.java
@@ -1,6 +1,7 @@
 package me.ian.workoutrecoder.service.impl;
 
 import me.ian.workoutrecoder.enums.ApplicationResponseCodeEnum;
+import me.ian.workoutrecoder.enums.MenuTypeEnum;
 import me.ian.workoutrecoder.exception.RestException;
 import me.ian.workoutrecoder.model.param.AddWorkoutMenuContentParam;
 import me.ian.workoutrecoder.model.po.WorkoutActionPO;
@@ -8,6 +9,7 @@ import me.ian.workoutrecoder.model.po.WorkoutMenuContentPO;
 import me.ian.workoutrecoder.model.po.WorkoutMenuPO;
 import me.ian.workoutrecoder.repository.WorkoutActionRepository;
 import me.ian.workoutrecoder.repository.WorkoutMenuContentRepository;
+import me.ian.workoutrecoder.repository.WorkoutMenuRepository;
 import me.ian.workoutrecoder.service.WorkoutMenuContentService;
 import org.springframework.stereotype.Service;
 
@@ -15,16 +17,24 @@ import org.springframework.stereotype.Service;
 public class WorkoutMenuContentServiceImpl implements WorkoutMenuContentService {
     private final WorkoutMenuContentRepository workoutMenuContentRepository;
     private final WorkoutActionRepository workoutActionRepository;
+    private final WorkoutMenuRepository workoutMenuRepository;
 
-    public WorkoutMenuContentServiceImpl(WorkoutMenuContentRepository workoutMenuContentRepository, WorkoutActionRepository workoutActionRepository) {
+    public WorkoutMenuContentServiceImpl(WorkoutMenuContentRepository workoutMenuContentRepository,
+                                         WorkoutActionRepository workoutActionRepository,
+                                         WorkoutMenuRepository workoutMenuRepository) {
         this.workoutMenuContentRepository = workoutMenuContentRepository;
         this.workoutActionRepository = workoutActionRepository;
+        this.workoutMenuRepository = workoutMenuRepository;
     }
 
     @Override
     public boolean addMenuContent(Integer userId, Integer menuId, AddWorkoutMenuContentParam param) {
         if (!isActionBelongUser(userId, param.getActionId())) {
             throw new RestException(ApplicationResponseCodeEnum.PARAMETER_WRONG.getCode(), "Not your action");
+        }
+
+        if (param.getDay() != null) {
+            this.checkoutMenuIsWeekly(menuId);
         }
 
         WorkoutMenuPO workoutMenuPO = new WorkoutMenuPO();
@@ -52,5 +62,12 @@ public class WorkoutMenuContentServiceImpl implements WorkoutMenuContentService 
     private boolean isActionBelongUser(Integer userId, Integer actionId) {
         WorkoutActionPO workoutActionPO = workoutActionRepository.findById(actionId).orElseThrow(() -> new RestException(ApplicationResponseCodeEnum.PARAMETER_WRONG.getCode()));
         return workoutActionPO.getUserId().equals(userId);
+    }
+
+    private void checkoutMenuIsWeekly(Integer menuId) {
+        WorkoutMenuPO workoutMenuPO = workoutMenuRepository.findById(menuId).orElseThrow(() -> new RestException(ApplicationResponseCodeEnum.DATA_NOT_EXIST.getCode()));
+        if (!workoutMenuPO.getType().equals(MenuTypeEnum.WEEKLY.getCode())) {
+            throw new RestException(ApplicationResponseCodeEnum.PARAMETER_WRONG.getCode(), "Not weekly menu");
+        }
     }
 }
